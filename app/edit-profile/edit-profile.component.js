dff0dfca6a344a8b6d7432a38ff246deeeb70286
Fabian Hoyos <fabian.hoyos@grability.com>
2016-09-04 23:33:27 -0500

    adjust logic -view

--- app/edit-profile/edit-profile.component.js
+++ app/edit-profile/edit-profile.component.js
@@ -9,15 +9,50 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var router_1 = require('@angular/router');
 var authentication_service_1 = require('../services/authentication.service');
 var user_1 = require('../user');
 var EditProfileComponent = (function () {
-    function EditProfileComponent(authService) {
+    function EditProfileComponent(router, authService) {
+        this.router = router;
         this.authService = authService;
-        this.user = user_1.User;
+        this.selectUser = user_1.User();
     }
+    EditProfileComponent.prototype.getUser = function () {
+        var _this = this;
+        this.authService.getUserProfile(localStorage.getItem('cedula_user')).subscribe(function (response) {
+            localStorage.setItem('cedula_user', response.cedula);
+            localStorage.setItem('name', response.nombre);
+            localStorage.setItem('type_user', response.tipo);
+            _this.selectUser = response;
+        }, function (error) {
+            alert(error.text());
+            console.log(error.text());
+            ;
+        });
+    };
+    EditProfileComponent.prototype.updateUser = function (cedula, nombre, email, password, confirmPassword, telefono) {
+        var _this = this;
+        var user = new user_1.User();
+        user.cedula = cedula;
+        user.nombre = nombre;
+        user.email = email;
+        user.password = password;
+        user.confirmPassword = confirmPassword;
+        user.telefono = telefono;
+        user.tipo = localStorage.getItem('type_user');
+        this.authService.updateUser(user)
+            .subscribe(function (response) {
+            alert("Usuario actualizado");
+            _this.router.navigate(['/editProfile']);
+        }, function (error) {
+            alert(error.text());
+            console.log(error.text());
+            _this.router.navigate(['/editProfile']);
+        });
+    };
     EditProfileComponent.prototype.ngOnInit = function () {
-        //TODO - Consumir el servicio que trae la informacion del usuario
+        this.getUser();
     };
     EditProfileComponent = __decorate([
         core_1.Component({
@@ -28,7 +63,7 @@ var EditProfileComponent = (function () {
                 authentication_service_1.AuthenticationService
             ]
         }), 
-        __metadata('design:paramtypes', [authentication_service_1.AuthenticationService])
+        __metadata('design:paramtypes', [router_1.Router, authentication_service_1.AuthenticationService])
     ], EditProfileComponent);
     return EditProfileComponent;
 }());
