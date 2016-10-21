7fe3887d174f8a3b43dd02079c6fd1ff56a0d750
Fabian Hoyos <fabian.hoyos@grability.com>
2016-10-21 00:44:56 -0500

    mock tramites

--- app/tramite/inbox.component.js
+++ app/tramite/inbox.component.js
@@ -18,28 +18,53 @@ var InboxTramiteComponent = (function () {
         this.router = router;
         this.authService = authService;
         this.toastr = toastr;
+        this.selectTramite = [];
         this.sortByWordLength = function (a) {
             return a.name.length;
         };
         this.menus = localStorage.getItem("type_user") === 'Ciudadano' ? menu_mock_1.MENU_CDN : menu_mock_1.MENU_ADM;
+        this.selectTramite = JSON.parse(localStorage.getItem("tramiteInbox")) === null ? [] : JSON.parse(localStorage.getItem("tramiteInbox"));
+        var tramite = JSON.parse(localStorage.getItem("tramiteStored"));
+        var edit = localStorage.getItem("editcampoStoredId");
+        if (edit === "true") {
+            localStorage.setItem("editcampoStoredId", "false");
+            var index = +localStorage.getItem("campoStoredId");
+            if (index > -1) {
+                index = index - 1;
+                this.selectTramite[index].nombre = tramite.nombre;
+                this.selectTramite[index].descripcion = tramite.descripcion;
+                this.selectTramite[index].campos = tramite.campos;
+                this.selectTramite[index].id = tramite.id;
+            }
+        }
+        else {
+            if (tramite != null) {
+                console.log("stored " + tramite.nombre);
+                this.selectTramite.push(tramite);
+                localStorage.setItem("tramiteStored", null);
+            }
+        }
     }
     InboxTramiteComponent.prototype.removeItem = function (item) {
-        this.toastr.info("Eliminar: " + item.id, 'Alerta');
-        console.log("Remove: ", item.id);
+        this.toastr.info("Eliminado: " + item.nombre, 'Alerta');
+        console.log("Remove: ", item.nombre);
+        var index = this.selectTramite.indexOf(item, 0);
+        if (index > -1) {
+            this.selectTramite.splice(index, 1);
+        }
+        localStorage.setItem("tramiteInbox", JSON.stringify(this.selectTramite));
     };
     InboxTramiteComponent.prototype.editItem = function (item) {
-        this.toastr.info("Editar: " + item.id, 'Alerta');
-        var campo = [];
-        localStorage.setItem("campos", JSON.stringify(campo));
-        localStorage.setItem("tramite", "");
-        localStorage.setItem("descripcion", "");
-        localStorage.setItem("edit", 'false');
-        localStorage.setItem("campoid", "");
-        localStorage.setItem("type", "");
-        localStorage.setItem("campo", "");
+        this.toastr.info("Editar: " + item.nombre, 'Alerta');
+        localStorage.setItem("tramiteInbox", JSON.stringify(this.selectTramite));
+        localStorage.setItem("tramite", item.nombre);
+        localStorage.setItem("descripcion", item.descripcion);
+        localStorage.setItem("campos", JSON.stringify(item.campos));
+        localStorage.setItem("campoStoredId", item.id);
+        localStorage.setItem("editcampoStoredId", "true");
         var link = ['/procedure'];
         this.router.navigate(link);
-        console.log("Edit: ", item.id);
+        console.log("Edit: ", item.nombre);
     };
     InboxTramiteComponent.prototype.newItem = function () {
         this.toastr.info("Nuevo Trámite", 'Alerta');
@@ -51,31 +76,27 @@ var InboxTramiteComponent = (function () {
         localStorage.setItem("campoid", "");
         localStorage.setItem("type", "");
         localStorage.setItem("campo", "");
+        localStorage.setItem("editcampoStoredId", "false");
+        localStorage.setItem("campoStoredId", this.selectTramite.length.toString());
+        localStorage.setItem("tramiteInbox", JSON.stringify(this.selectTramite));
         var link = ['/procedure'];
         this.router.navigate(link);
         console.log("Nuevo Trámite: ");
     };
     InboxTramiteComponent.prototype.ngOnInit = function () {
-        //alert("Bandeja Trámites");
-        this.selectTramite = [
-            { descripcion: 'descripicion del tramite1', nombre: 'tramite1', id: 1 },
-            { descripcion: 'descripicion del tramite2', nombre: 'tramite2', id: 2 },
-            { descripcion: 'descripicion del tramite3', nombre: 'tramite3', id: 3 },
-            { descripcion: 'descripicion del tramite4', nombre: 'tramite4', id: 4 }
-        ];
-        this.username = "Administrador";
-        //this.getAllTramites();
     };
     InboxTramiteComponent.prototype.getAllTramites = function () {
-        var _this = this;
-        this.authService.getAllTramites()
-            .subscribe(function (response) {
-            _this.selectTramite = response;
-        }, function (error) {
-            _this.toastr.error('hay un error', 'Alerta');
-            _this.toastr.error(error.text(), 'Alerta');
-            console.log(error.text());
-        });
+        /* this.authService.getAllTramites()
+         .subscribe(
+         response => {
+         this.selectTramite = response;
+         },
+         error => {
+         this.toastr.error('hay un error', 'Alerta');
+         this.toastr.error(error.text(), 'Alerta');
+         console.log(error.text());
+         }
+         );*/
     };
     InboxTramiteComponent.prototype.onSelect = function (hero) {
         this.router.navigate([hero.id]);
