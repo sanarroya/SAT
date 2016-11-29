9073dd6885c30e0fe2f6c6c20ad740e1eb71b6f9
Fabian Hoyos <fabian.hoyos@grability.com>
2016-11-28 19:23:28 -0500

    test

---
+++ app/camposSolicitud.js
@@ -0,0 +1,8 @@
+"use strict";
+var camposolicitud = (function () {
+    function camposolicitud() {
+    }
+    return camposolicitud;
+}());
+exports.camposolicitud = camposolicitud;
+//# sourceMappingURL=camposSolicitud.js.map
